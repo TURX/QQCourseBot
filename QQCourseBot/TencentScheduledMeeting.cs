765d2b9b8eb0fc3921ccf9845d34adfb7111266c
TURX <turx2003@gmail.com>
2020-03-17 01:36:10 +0800

    1.3: auto join Tencent Meeting

---
+++ QQCourseBot/TencentScheduledMeeting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace QQCourseBot
+{
+    public class TencentScheduledMeeting
+    {
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public string Url;
+    }
+}
